779431d8a35d420832f35c6181907e2537ff4a1f
Kenny Pflug <kenny.pflug@live.de>
2016-06-18 14:25:18 +0200

    added project structure

---
+++ Code/Light.DependencyInjection/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("Light.DependencyInjection")]
+[assembly: AssemblyDescription("A lightweight DI container for .NET - because there aren't enough yet.")]
+[assembly: AssemblyCompany("Kenny Pflug")]
+[assembly: AssemblyProduct("Light.DependencyInjection")]
+[assembly: AssemblyCopyright("Copyright © Kenny Pflug 2016")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: NeutralResourcesLanguage("en")]
