e074fd432ed74c35c6442869e8bd6125cc25cd50
Kenny Pflug <kenny.pflug@live.de>
2016-09-21 18:38:04 +0200

    Removed KeyComparer from SynchronizeDictionaryOptions

--- Code/Light.DependencyInjection/Multithreading/SynchronizedDictionaryOptions.cs
+++ Code/Light.DependencyInjection/Multithreading/SynchronizedDictionaryOptions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Light.GuardClauses;
 
 namespace Light.DependencyInjection.Multithreading
@@ -8,7 +7,6 @@ namespace Light.DependencyInjection.Multithreading
     {
         private IGrowBucketContainerStrategy<TKey, TValue> _growContainerStrategy;
         public static readonly IGrowBucketContainerStrategy<TKey, TValue> DefaultGrowContainerStrategy = new PrimeNumberLinearStrategy<TKey, TValue>();
-        private IEqualityComparer<TKey> _keyComparer;
 
         public IGrowBucketContainerStrategy<TKey, TValue> GrowContainerStrategy
         {
@@ -20,22 +18,11 @@ namespace Light.DependencyInjection.Multithreading
             }
         }
 
-        public IEqualityComparer<TKey> KeyComparer
-        {
-            get { return _keyComparer; }
-            set
-            {
-                value.MustNotBeNull(nameof(value));
-                _keyComparer = value;
-            }
-        }
-
         public static SynchronizedDictionaryOptions<TKey, TValue> Create()
         {
             return new SynchronizedDictionaryOptions<TKey, TValue>
                    {
-                       _growContainerStrategy = DefaultGrowContainerStrategy,
-                       _keyComparer = EqualityComparer<TKey>.Default
+                       _growContainerStrategy = DefaultGrowContainerStrategy
                    };
         }
     }
