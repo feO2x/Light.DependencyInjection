8ab0692915f4701cd705b0544126bb285cf756b6
Kenny Pflug <kenny.pflug@live.de>
2016-09-25 16:31:12 +0200

    moved OptionsConstructorWithOneParameter test to BaseOptionsTests

--- Code/Light.DependencyInjection.Tests/ResolveTests.cs
+++ Code/Light.DependencyInjection.Tests/ResolveTests.cs
@@ -93,13 +93,7 @@ namespace Light.DependencyInjection.Tests
 
         
 
-        [Fact(DisplayName = "Clients must be able to choose a constructor with a single parameter that the DI container uses to instantiate the target type.")]
-        public void OptionsConstructorWithOneParameter()
-        {
-            Container.RegisterTransient<D>(options => options.UseConstructorWithParameter<IList<int>>());
-
-            Container.Registrations.Should().ContainSingle(registration => ((ConstructorInstantiationInfo) registration.TypeCreationInfo.InstantiationInfo).ConstructorInfo == typeof(D).GetTypeInfo().DeclaredConstructors.First(c => c.GetParameters().Length == 1));
-        }
+        
 
         [Fact(DisplayName = "Clients must be able to choose a constructor with two parameters that the DI container uses to instantiate the target type.")]
         public void OptionsConstructorWithTwoParameters()
@@ -296,6 +290,14 @@ namespace Light.DependencyInjection.Tests
             Register<D>(options => options.UseDefaultConstructor());
 
             Container.Registrations.Should().ContainSingle(registration => ((ConstructorInstantiationInfo)registration.TypeCreationInfo.InstantiationInfo).ConstructorInfo == typeof(D).GetTypeInfo().DeclaredConstructors.First());
+        }
+
+        [Fact(DisplayName = "Clients must be able to choose a constructor with a single parameter that the DI container uses to instantiate the target type.")]
+        public void OptionsConstructorWithOneParameter()
+        {
+            Register<D>(options => options.UseConstructorWithParameter<IList<int>>());
+
+            Container.Registrations.Should().ContainSingle(registration => ((ConstructorInstantiationInfo)registration.TypeCreationInfo.InstantiationInfo).ConstructorInfo == typeof(D).GetTypeInfo().DeclaredConstructors.First(c => c.GetParameters().Length == 1));
         }
     }
 
