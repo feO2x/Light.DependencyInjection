41bc833cbfc29e4cfaa75495f4ddb49c066d06e7
Kenny Pflug <kenny.pflug@live.de>
2016-10-14 11:42:07 +0200

    PropertyInjection now uses dynamically compiled code to set a value on a property

--- Code/Light.DependencyInjection/TypeConstruction/PropertyInjection.cs
+++ Code/Light.DependencyInjection/TypeConstruction/PropertyInjection.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Linq.Expressions;
 using System.Reflection;
 using Light.DependencyInjection.FrameworkExtensions;
 using Light.GuardClauses;
@@ -9,6 +10,7 @@ namespace Light.DependencyInjection.TypeConstruction
     public sealed class PropertyInjection : InstanceInjection
     {
         public readonly PropertyInfo PropertyInfo;
+        private readonly Action<object, object> _setValueAction;
 
         public PropertyInjection(PropertyInfo propertyInfo, string childValueRegistrationName = null) 
             : base(propertyInfo.Name, propertyInfo.PropertyType, propertyInfo.DeclaringType, childValueRegistrationName)
@@ -16,6 +18,19 @@ namespace Light.DependencyInjection.TypeConstruction
             CheckPropertyInfo(propertyInfo);
 
             PropertyInfo = propertyInfo;
+            _setValueAction = CreateSetValueAction(propertyInfo);
+        }
+
+        private static Action<object, object> CreateSetValueAction(PropertyInfo propertyInfo)
+        {
+            var instanceParameter = Expression.Parameter(typeof(object), "instance");
+            var valueParameter = Expression.Parameter(typeof(object), "value");
+
+            var instanceDowncast = Expression.Convert(instanceParameter, propertyInfo.DeclaringType);
+            var valueDowncast = Expression.Convert(valueParameter, propertyInfo.PropertyType);
+
+            var callSetter = Expression.Call(instanceDowncast, propertyInfo.SetMethod, valueDowncast);
+            return Expression.Lambda<Action<object, object>>(callSetter, instanceParameter, valueParameter).Compile();
         }
 
         [Conditional(Check.CompileAssertionsSymbol)]
@@ -29,7 +44,7 @@ namespace Light.DependencyInjection.TypeConstruction
 
         public override void InjectValue(object instance, object value)
         {
-            PropertyInfo.SetValue(instance, value);
+            _setValueAction(instance, value);
         }
 
         protected override InstanceInjection CloneForClosedConstructedGenericTypeInternal(Type closedConstructedGenericType, TypeInfo closedConstructedGenericTypeInfo)
