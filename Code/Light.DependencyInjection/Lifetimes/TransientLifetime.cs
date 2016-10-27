054f79b660428f6f51c411ea393dfe4c01c5dfff
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 00:14:41 +0200

    added XML comment for TransientLifetime

--- Code/Light.DependencyInjection/Lifetimes/TransientLifetime.cs
+++ Code/Light.DependencyInjection/Lifetimes/TransientLifetime.cs
@@ -2,15 +2,27 @@
 
 namespace Light.DependencyInjection.Lifetimes
 {
+    /// <summary>
+    ///     Represents a lifetime that always creates new instances.
+    /// </summary>
     public sealed class TransientLifetime : Lifetime
     {
+        /// <summary>
+        ///     Gets the singleton instance of this lifetime.
+        /// </summary>
         public static readonly TransientLifetime Instance = new TransientLifetime();
 
+        /// <summary>
+        ///     Returns a new instance.
+        /// </summary>
         public override object GetInstance(ResolveContext context)
         {
             return context.CreateInstance();
         }
 
+        /// <summary>
+        ///     Return reference to itself.
+        /// </summary>
         public override Lifetime ProvideInstanceForResolvedGenericTypeDefinition()
         {
             return this;
