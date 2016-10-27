266200f3814d98ecc72c76fde052d58a2eab355d
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 00:10:46 +0200

    added XML comment for ScopedLifetime

--- Code/Light.DependencyInjection/Lifetimes/ScopedLifetime.cs
+++ Code/Light.DependencyInjection/Lifetimes/ScopedLifetime.cs
@@ -2,16 +2,28 @@
 
 namespace Light.DependencyInjection.Lifetimes
 {
+    /// <summary>
+    ///     Represents a lifetime that creates instances once per container lifetime.
+    /// </summary>
     public sealed class ScopedLifetime : Lifetime
     {
+        /// <summary>
+        ///     Gets the singleton instance of this lifetime.
+        /// </summary>
         public static readonly ScopedLifetime Instance = new ScopedLifetime();
 
+        /// <summary>
+        ///     Creates a new instance if the is non for the current container scope, otherwise the existing instance will be returned.
+        /// </summary>
         public override object GetInstance(ResolveContext context)
         {
             return context.Container.Scope.GetOrAddScopedInstance(context.Registration.TypeKey,
                                                                   context.CreateInstance);
         }
 
+        /// <summary>
+        ///     Returns reference to itself.
+        /// </summary>
         public override Lifetime ProvideInstanceForResolvedGenericTypeDefinition()
         {
             return this;
