d5151fef255afed25e4b04e333bae5b06bb3f71e
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 00:08:29 +0200

    added XML comments for PerThreadLifetime

--- Code/Light.DependencyInjection/Lifetimes/PerThreadLifetime.cs
+++ Code/Light.DependencyInjection/Lifetimes/PerThreadLifetime.cs
@@ -3,10 +3,16 @@ using Light.DependencyInjection.Services;
 
 namespace Light.DependencyInjection.Lifetimes
 {
+    /// <summary>
+    ///     Represents a lifetime that creates instances once per thread.
+    /// </summary>
     public sealed class PerThreadLifetime : Lifetime
     {
         private readonly ThreadLocal<object> _threadLocal = new ThreadLocal<object>();
 
+        /// <summary>
+        ///     Creates a new instance if it does not exist for the current thread, else the existing instance will be returned.
+        /// </summary>
         public override object GetInstance(ResolveContext context)
         {
             if (_threadLocal.IsValueCreated == false)
@@ -15,6 +21,9 @@ namespace Light.DependencyInjection.Lifetimes
             return _threadLocal.Value;
         }
 
+        /// <summary>
+        ///     Returns a new instance of this lifetime.
+        /// </summary>
         public override Lifetime ProvideInstanceForResolvedGenericTypeDefinition()
         {
             return new PerThreadLifetime();
