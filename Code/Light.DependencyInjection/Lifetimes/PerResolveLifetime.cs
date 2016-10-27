41667efb591aaf396488b518686787139dd550a4
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 00:06:55 +0200

    added XML comments for PerResolveLifetime

--- Code/Light.DependencyInjection/Lifetimes/PerResolveLifetime.cs
+++ Code/Light.DependencyInjection/Lifetimes/PerResolveLifetime.cs
@@ -2,15 +2,27 @@
 
 namespace Light.DependencyInjection.Lifetimes
 {
+    /// <summary>
+    ///     Represents a lifetime that creates instances once per resolve.
+    /// </summary>
     public sealed class PerResolveLifetime : Lifetime
     {
+        /// <summary>
+        ///     Gets the singleton instance of this lifetime.
+        /// </summary>
         public static readonly PerResolveLifetime Instance = new PerResolveLifetime();
 
+        /// <summary>
+        ///     Creates a new instance if it was not created before during a Resolve call, otherwise returns the existing instance.
+        /// </summary>
         public override object GetInstance(ResolveContext context)
         {
             return context.GetOrCreatePerResolveInstance();
         }
 
+        /// <summary>
+        ///     Returns reference to itself.
+        /// </summary>
         public override Lifetime ProvideInstanceForResolvedGenericTypeDefinition()
         {
             return this;
