3fcd5fdc9a163c912d5262cd3dcf72d073e3a318
Kenny Pflug <kenny.pflug@live.de>
2016-10-27 10:54:20 +0200

    updated assembly info for v0.1

--- Code/Light.DependencyInjection/Properties/AssemblyInfo.cs
+++ Code/Light.DependencyInjection/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Resources;
 
 [assembly: AssemblyTitle("Light.DependencyInjection")]
-[assembly: AssemblyDescription("A lightweight DI container for .NET - because there aren't enough yet.")]
+[assembly: AssemblyDescription("A lightweight DI container for .NET.")]
 [assembly: AssemblyCompany("Kenny Pflug")]
 [assembly: AssemblyProduct("Light.DependencyInjection")]
 [assembly: AssemblyCopyright("Copyright © Kenny Pflug 2016")]
