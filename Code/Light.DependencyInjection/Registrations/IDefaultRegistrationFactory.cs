7121e8a23942f260d62900468d0b4e326f1d6f6c
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 07:18:27 +0200

    added XML comments for IDefaultRegistrationFactory

--- Code/Light.DependencyInjection/Registrations/IDefaultRegistrationFactory.cs
+++ Code/Light.DependencyInjection/Registrations/IDefaultRegistrationFactory.cs
@@ -2,8 +2,15 @@
 
 namespace Light.DependencyInjection.Registrations
 {
+    /// <summary>
+    ///     Represents the abstraction for creating a default registration for a resolved type
+    ///     that has not been registered with the DI container before.
+    /// </summary>
     public interface IDefaultRegistrationFactory
     {
+        /// <summary>
+        ///     Creates the default registration for the specified type info.
+        /// </summary>
         Registration CreateDefaultRegistration(TypeCreationInfo typeCreationInfo);
     }
 }
