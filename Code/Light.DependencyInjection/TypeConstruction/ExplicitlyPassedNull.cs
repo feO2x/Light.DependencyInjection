880a923a1f9bd9c66b6934e1084ee9e5f3f43533
Kenny Pflug <kenny.pflug@live.de>
2016-10-29 00:40:52 +0200

    added XML comments for ExplicitlyPassedNull

--- Code/Light.DependencyInjection/TypeConstruction/ExplicitlyPassedNull.cs
+++ Code/Light.DependencyInjection/TypeConstruction/ExplicitlyPassedNull.cs
@@ -1,7 +1,13 @@
 ﻿namespace Light.DependencyInjection.TypeConstruction
 {
+    /// <summary>
+    ///     Indicates that the client wants to inject null via <see cref="DependencyOverrides" />.
+    /// </summary>
     public sealed class ExplicitlyPassedNull
     {
+        /// <summary>
+        ///     Gets the singleton instance of <see cref="ExplicitlyPassedNull" />.
+        /// </summary>
         public static readonly ExplicitlyPassedNull Instance = new ExplicitlyPassedNull();
 
         private ExplicitlyPassedNull() { }
