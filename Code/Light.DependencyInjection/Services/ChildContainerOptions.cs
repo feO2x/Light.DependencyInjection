de8f3cf26da5ba06fcad86b46ba01db4dde0e1db
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 14:33:14 +0200

    added XML comments for ChildContainerOptions

--- Code/Light.DependencyInjection/Services/ChildContainerOptions.cs
+++ Code/Light.DependencyInjection/Services/ChildContainerOptions.cs
@@ -1,11 +1,31 @@
 ﻿namespace Light.DependencyInjection.Services
 {
+    /// <summary>
+    ///     Represents the options that configure how a child container is created.
+    /// </summary>
     public struct ChildContainerOptions
     {
+        /// <summary>
+        ///     Gets the value indicating whether an empty scope should be created that is not linked to the scope of the parent container.
+        /// </summary>
         public readonly bool CreateEmptyScope;
+
+        /// <summary>
+        ///     Gets the value indicating whether the mappings dictionary should be copied instead of passed by reference.
+        /// </summary>
         public readonly bool CreateCopyOfMappings;
+
+        /// <summary>
+        ///     Gets the value indicating whether a shallow copy of the container services should be passed to the child container instead of the same reference.
+        /// </summary>
         public readonly bool CloneContainerServices;
 
+        /// <summary>
+        ///     Creates a new instance of <see cref="ChildContainerOptions" />.
+        /// </summary>
+        /// <param name="createEmptyScope">The value indicating whether an empty scope should be created that is not linked to the scope of the parent container.</param>
+        /// <param name="createCopyOfMappings">The value indicating whether the mappings dictionary should be copied instead of passed by reference.</param>
+        /// <param name="cloneContainerServices">The value indicating whether a shallow copy of the container services should be passed to the child container instead of the same reference.</param>
         public ChildContainerOptions(bool createEmptyScope = false,
                                      bool createCopyOfMappings = false,
                                      bool cloneContainerServices = false)
@@ -15,6 +35,9 @@
             CloneContainerServices = cloneContainerServices;
         }
 
+        /// <summary>
+        ///     Gets the default child container options.
+        /// </summary>
         public static ChildContainerOptions Default => new ChildContainerOptions(false);
     }
 }
