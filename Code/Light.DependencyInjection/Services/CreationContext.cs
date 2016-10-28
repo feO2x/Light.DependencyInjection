063908b434c4100a2f65d3e0d7f0dbfb488e36a9
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 15:09:44 +0200

    added XML comments for CreationContext

--- Code/Light.DependencyInjection/Services/CreationContext.cs
+++ Code/Light.DependencyInjection/Services/CreationContext.cs
@@ -6,11 +6,29 @@ using Light.GuardClauses;
 
 namespace Light.DependencyInjection.Services
 {
+    /// <summary>
+    ///     Represents all information necessary to instantiate a registration.
+    /// </summary>
     public struct CreationContext
     {
+        /// <summary>
+        ///     Gets the <see cref="DependencyInjectionContainer" /> instance.
+        /// </summary>
         public readonly DependencyInjectionContainer Container;
+
+        /// <summary>
+        ///     Gets the parameter overrides for this resolve call (if present).
+        /// </summary>
         public readonly ParameterOverrides? ParameterOverrides;
+
+        /// <summary>
+        ///     Gets the registration that should be instantiated.
+        /// </summary>
         public readonly Registration Registration;
+
+        /// <summary>
+        ///     Gets the scope of this resolve call.
+        /// </summary>
         public readonly Lazy<Dictionary<TypeKey, object>> LazyResolveScope;
 
         private CreationContext(DependencyInjectionContainer container, ParameterOverrides? parameterOverrides)
@@ -32,6 +50,11 @@ namespace Light.DependencyInjection.Services
             LazyResolveScope = lazyResolveScope;
         }
 
+        /// <summary>
+        ///     Checks if the requested type key is part of the resolve scope. If not, then a recursive resolve is performed on the DI container.
+        /// </summary>
+        /// <param name="requestedTypeKey">The type key uniquely identifying the requested instance.</param>
+        /// <returns>The retrieved or resolved instance.</returns>
         public object ResolveChildValue(TypeKey requestedTypeKey)
         {
             object perResolveInstance;
@@ -41,6 +64,9 @@ namespace Light.DependencyInjection.Services
             return Container.ResolveRecursively(requestedTypeKey, this);
         }
 
+        /// <summary>
+        ///     Creates a <see cref="CreationContext" /> instance from the specified <see cref="ResolveContext" />.
+        /// </summary>
         public static CreationContext FromResolveContext(ResolveContext resolveContext, Lazy<Dictionary<TypeKey, object>> resolveScope)
         {
             return new CreationContext(resolveContext.Container,
@@ -49,6 +75,9 @@ namespace Light.DependencyInjection.Services
                                        resolveScope);
         }
 
+        /// <summary>
+        ///     Creates the <see cref="CreationContext" /> instance for the initial call to <see cref="DependencyInjectionContainer.ResolveRecursively" />.
+        /// </summary>
         public static CreationContext CreateInitial(DependencyInjectionContainer container, ParameterOverrides? parameterOverrides = null)
         {
             container.MustNotBeNull(nameof(container));
