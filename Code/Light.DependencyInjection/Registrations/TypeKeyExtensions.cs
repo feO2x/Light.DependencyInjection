fe33158d4b8f19aaa72b73d923d79c9a21754c27
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 07:16:59 +0200

    added XML comments for TypeKeyExtensions

--- Code/Light.DependencyInjection/Registrations/TypeKeyExtensions.cs
+++ Code/Light.DependencyInjection/Registrations/TypeKeyExtensions.cs
@@ -1,12 +1,22 @@
 ﻿namespace Light.DependencyInjection.Registrations
 {
+    /// <summary>
+    ///     Provides extension methods for the <see cref="TypeKey" /> struct.
+    /// </summary>
     public static class TypeKeyExtensions
     {
+        /// <summary>
+        ///     Gets the full registration name in the following format: "TypeKey.Type.FullName" with name "TypeKey.RegistrationName".
+        ///     The last part will be ommited if registration name is null.
+        /// </summary>
         public static string GetFullRegistrationName(this TypeKey typeKey)
         {
             return $"\"{typeKey.Type}\"{typeKey.GetWithRegistrationNameText()}";
         }
 
+        /// <summary>
+        ///     Gets the following text or an empty string depending on typeKey.RegistrationName being null: with name "TypeKey.RegistrationName".
+        /// </summary>
         public static string GetWithRegistrationNameText(this TypeKey typeKey)
         {
             return typeKey.RegistrationName == null ? "" : $" with name \"{typeKey.RegistrationName}\"";
