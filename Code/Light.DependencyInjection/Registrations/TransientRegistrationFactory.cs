40d8d7353e09b5fa67e3995123384b49495d4ff0
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 07:19:40 +0200

    added XML comment for TransientRegistrationFactory

--- Code/Light.DependencyInjection/Registrations/TransientRegistrationFactory.cs
+++ Code/Light.DependencyInjection/Registrations/TransientRegistrationFactory.cs
@@ -3,8 +3,14 @@ using Light.DependencyInjection.TypeConstruction;
 
 namespace Light.DependencyInjection.Registrations
 {
+    /// <summary>
+    ///     Represents an <see cref="IDefaultRegistrationFactory" /> that creates registrations with a transient lifetime.
+    /// </summary>
     public sealed class TransientRegistrationFactory : IDefaultRegistrationFactory
     {
+        /// <summary>
+        ///     Creates a new registration with a transient lifetime for the specified <paramref name="typeCreationInfo" />.
+        /// </summary>
         public Registration CreateDefaultRegistration(TypeCreationInfo typeCreationInfo)
         {
             return new Registration(TransientLifetime.Instance, typeCreationInfo);
