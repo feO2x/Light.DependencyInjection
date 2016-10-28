2c73de593acafd118b10a959c031f08d7d5c70be
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 14:29:07 +0200

    added XML comments for IChildRegistrationNameOptions<TRegistrationOptions>

--- Code/Light.DependencyInjection/Registrations/ChildRegistrationNameOptions.cs
+++ Code/Light.DependencyInjection/Registrations/ChildRegistrationNameOptions.cs
@@ -1,13 +1,24 @@
+using System;
 using Light.DependencyInjection.TypeConstruction;
 using Light.GuardClauses;
 
 namespace Light.DependencyInjection.Registrations
 {
+    /// <summary>
+    ///     Represents the options for setting a target registration name with a fluent API.
+    /// </summary>
+    /// <typeparam name="TRegistrationOptions">The registration options returned by the fluent API.</typeparam>
     public sealed class ChildRegistrationNameOptions<TRegistrationOptions> : IChildRegistrationNameOptions<TRegistrationOptions> where TRegistrationOptions : class, IBaseRegistrationOptionsForType<TRegistrationOptions>
     {
         private readonly TRegistrationOptions _registrationOptions;
         private readonly ISetTargetRegistrationName _targetDependency;
 
+        /// <summary>
+        ///     Initializes a new instance of <see cref="ChildRegistrationNameOptions{TRegistrationOptions}" />
+        /// </summary>
+        /// <param name="registrationOptions">The registration options that should be returned by the fluent API.</param>
+        /// <param name="targetDependency">The dependency whose target registration name should be changed.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="registrationOptions" /> or <paramref name="targetDependency" /> is null.</exception>
         public ChildRegistrationNameOptions(TRegistrationOptions registrationOptions, ISetTargetRegistrationName targetDependency)
         {
             registrationOptions.MustNotBeNull(nameof(registrationOptions));
@@ -17,6 +28,7 @@ namespace Light.DependencyInjection.Registrations
             _targetDependency = targetDependency;
         }
 
+        /// <inheritdoc />
         public TRegistrationOptions WithName(string childValueRegistrationName)
         {
             _targetDependency.TargetRegistrationName = childValueRegistrationName;
--- Code/Light.DependencyInjection/Registrations/IChildRegistrationNameOptions.cs
+++ Code/Light.DependencyInjection/Registrations/IChildRegistrationNameOptions.cs
@@ -1,7 +1,14 @@
 ﻿namespace Light.DependencyInjection.Registrations
 {
+    /// <summary>
+    ///     Represents the abstraction for setting a target registration name with a fluent API.
+    /// </summary>
+    /// <typeparam name="TRegistrationOptions">The registration options returned by the fluent API.</typeparam>
     public interface IChildRegistrationNameOptions<out TRegistrationOptions> where TRegistrationOptions : class, IBaseRegistrationOptionsForType<TRegistrationOptions>
     {
+        /// <summary>
+        ///     Sets the target registration name.
+        /// </summary>
         TRegistrationOptions WithName(string childValueRegistrationName);
     }
 }
