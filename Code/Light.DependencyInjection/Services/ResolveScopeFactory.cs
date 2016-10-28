ef1056d358f00b6a5cb71eb1c4813a705a0473d0
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 15:30:31 +0200

    added XML comments for ResolveScopeFactory

--- Code/Light.DependencyInjection/Services/ResolveScopeFactory.cs
+++ Code/Light.DependencyInjection/Services/ResolveScopeFactory.cs
@@ -6,6 +6,9 @@ using Light.GuardClauses;
 
 namespace Light.DependencyInjection.Services
 {
+    /// <summary>
+    ///     Represents a factory that creates lazy scopes for each Resolve call.
+    /// </summary>
     public sealed class ResolveScopeFactory
     {
         private static readonly Func<Dictionary<TypeKey, object>> CreateDictionaryDelegate;
@@ -16,6 +19,9 @@ namespace Light.DependencyInjection.Services
             CreateDictionaryDelegate = CreateDictionary;
         }
 
+        /// <summary>
+        ///     Gets or sets the lazy thread safety mode used for created scopes. This value defaults to LazyThreadSafetyMode.None.
+        /// </summary>
         public LazyThreadSafetyMode LazyThreadSafetyMode
         {
             get { return _lazyThreadSafetyMode; }
@@ -26,6 +32,9 @@ namespace Light.DependencyInjection.Services
             }
         }
 
+        /// <summary>
+        ///     Creates a lazy resolve scope.
+        /// </summary>
         public Lazy<Dictionary<TypeKey, object>> CreateLazyScope()
         {
             return new Lazy<Dictionary<TypeKey, object>>(CreateDictionaryDelegate, _lazyThreadSafetyMode);
