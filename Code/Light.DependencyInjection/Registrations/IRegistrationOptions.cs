76df7487f26f5c8ca692743999d5f8c93dc2f154
Kenny Pflug <kenny.pflug@live.de>
2016-10-28 12:15:05 +0200

    added XML comments for IRegistrationOptionsForType

--- Code/Light.DependencyInjection/Registrations/IRegistrationOptions.cs
+++ Code/Light.DependencyInjection/Registrations/IRegistrationOptions.cs
@@ -199,18 +199,64 @@ namespace Light.DependencyInjection.Registrations
         TConcreteOptions ResolveAllForInstantiationParameter<TParameter>();
     }
 
+    /// <summary>
+    ///     Represents the abstraction for configuring registrations that are instantiated by the DI container.
+    /// </summary>
     public interface IRegistrationOptionsForType : IBaseRegistrationOptionsForType<IRegistrationOptionsForType>
     {
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith(Func<object> createInstance);
+
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith<TParameter>(Func<TParameter, object> createInstance);
+
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith<T1, T2>(Func<T1, T2, object> createInstance);
+
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith<T1, T2, T3>(Func<T1, T2, T3, object> createInstance);
+
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith<T1, T2, T3, T4>(Func<T1, T2, T3, T4, object> createInstance);
+
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith<T1, T2, T3, T4, T5>(Func<T1, T2, T3, T4, T5, object> createInstance);
+
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith<T1, T2, T3, T4, T5, T6>(Func<T1, T2, T3, T4, T5, T6, object> createInstance);
+
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith<T1, T2, T3, T4, T5, T6, T7>(Func<T1, T2, T3, T4, T5, T6, T7, object> createInstance);
+
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith<T1, T2, T3, T4, T5, T6, T7, T8>(Func<T1, T2, T3, T4, T5, T6, T7, T8, object> createInstance);
+
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith<T1, T2, T3, T4, T5, T6, T7, T8, T9>(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, object> createInstance);
+
+        /// <summary>
+        ///     Configures the registration to use the specified delegate to instantiate the target type.
+        /// </summary>
         IRegistrationOptionsForType InstantiateWith<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, object> createInstance);
     }
 
