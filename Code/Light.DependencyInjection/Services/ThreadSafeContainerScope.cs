ec6ef60595e80f33af434115bd3b71905b80ff03
Kenny Pflug <kenny.pflug@live.de>
2016-10-29 01:04:46 +0200

    added XML comments for ThreadSafeContainerScope

--- Code/Light.DependencyInjection/Services/ThreadSafeContainerScope.cs
+++ Code/Light.DependencyInjection/Services/ThreadSafeContainerScope.cs
@@ -5,13 +5,21 @@ using Light.DependencyInjection.Registrations;
 
 namespace Light.DependencyInjection.Services
 {
+    /// <summary>
+    ///     Represents a thread-safe version of the <see cref="ContainerScope" />.
+    /// </summary>
     public sealed class ThreadSafeContainerScope : ContainerScope
     {
         private readonly object _lock = new object();
 
+        /// <summary>
+        ///     Initializes a new instance of <see cref="ThreadSafeContainerScope" />.
+        /// </summary>
+        /// <param name="parentScope">The parent scope associated with the new one.</param>
         public ThreadSafeContainerScope(ContainerScope parentScope = null)
             : base(parentScope) { }
 
+        /// <inheritdoc />
         public override IReadOnlyList<IDisposable> DisposableObjects
         {
             get
@@ -23,6 +31,7 @@ namespace Light.DependencyInjection.Services
             }
         }
 
+        /// <inheritdoc />
         public override bool TryGetScopedInstance(TypeKey typeKey, out object instance)
         {
             lock (_lock)
@@ -31,6 +40,7 @@ namespace Light.DependencyInjection.Services
             }
         }
 
+        /// <inheritdoc />
         public override bool GetOrAddScopedInstance(TypeKey typeKey, Func<object> createInstance, out object instance)
         {
             lock (_lock)
@@ -39,6 +49,7 @@ namespace Light.DependencyInjection.Services
             }
         }
 
+        /// <inheritdoc />
         public override bool TryAddDisposable(object instance)
         {
             lock (_lock)
@@ -47,6 +58,7 @@ namespace Light.DependencyInjection.Services
             }
         }
 
+        /// <inheritdoc />
         public override void Dispose()
         {
             lock (_lock)
