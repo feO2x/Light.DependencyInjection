90e7bf965bb129d5cb9d6f6119b62c487c01dfcc
Kenny Pflug <kenny.pflug@live.de>
2016-10-29 07:56:36 +0200

    added XML comments for ImmutableListExtensions

--- Code/Light.DependencyInjection/DataStructures/ImmutableListExtensions.cs
+++ Code/Light.DependencyInjection/DataStructures/ImmutableListExtensions.cs
@@ -3,8 +3,21 @@ using Light.GuardClauses;
 
 namespace Light.DependencyInjection.DataStructures
 {
+    /// <summary>
+    ///     Provides extension methods for the <see cref="ImmutableList{T}" /> class when the item type is <see cref="HashEntry{TKey,TValue}" />.
+    /// </summary>
     public static class ImmutableListExtensions
     {
+        /// <summary>
+        ///     Tries to retrieve the value with the given key from the immutable list.
+        /// </summary>
+        /// <typeparam name="TKey">The key type.</typeparam>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <param name="immutableList">The immutable list that is searched.</param>
+        /// <param name="key">The key that identifies the value.</param>
+        /// <param name="value">The retrieved value.</param>
+        /// <returns>True if the value could be retrieved, else false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="immutableList" /> is null.</exception>
         public static bool TryFind<TKey, TValue>(this ImmutableList<HashEntry<TKey, TValue>> immutableList, TKey key, out TValue value) where TKey : IEquatable<TKey>
         {
             immutableList.MustNotBeNull(nameof(immutableList));
@@ -22,6 +35,15 @@ namespace Light.DependencyInjection.DataStructures
             return false;
         }
 
+        /// <summary>
+        ///     Checks if the immutable list contains a hash entry with the specified key.
+        /// </summary>
+        /// <typeparam name="TKey">The key type.</typeparam>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <param name="immutableList">The immutable list that is searched.</param>
+        /// <param name="key">The key that identifies the value.</param>
+        /// <returns>True if the immutable list contains an entry with the specified key, else false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="immutableList" /> is null.</exception>
         public static bool ContainsEntryWithKey<TKey, TValue>(this ImmutableList<HashEntry<TKey, TValue>> immutableList, TKey key) where TKey : IEquatable<TKey>
         {
             immutableList.MustNotBeNull(nameof(immutableList));
@@ -35,6 +57,15 @@ namespace Light.DependencyInjection.DataStructures
             return false;
         }
 
+        /// <summary>
+        ///     Returns the index of the entry with the specified key, or -1 if no such entry exists in the immutable list.
+        /// </summary>
+        /// <typeparam name="TKey">The key type.</typeparam>
+        /// <typeparam name="TValue">The value type.</typeparam>
+        /// <param name="immutableList">The immutable list that is searched.</param>
+        /// <param name="key">The key that identifies the value.</param>
+        /// <returns>The index of the entry with the specified key, or -1 if no such entry exists in the immutable list.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="immutableList" /> is null.</exception>
         public static int IndexOfEntry<TKey, TValue>(this ImmutableList<HashEntry<TKey, TValue>> immutableList, TKey key) where TKey : IEquatable<TKey>
         {
             immutableList.MustNotBeNull(nameof(immutableList));
