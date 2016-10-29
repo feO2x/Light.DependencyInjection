ca3ccde92b0c543ade48f0943f883927d5d7c8fe
Kenny Pflug <kenny.pflug@live.de>
2016-10-29 11:41:21 +0200

    added XML comments for RegistrationDictionaryOptions

--- Code/Light.DependencyInjection/DataStructures/RegistrationDictionaryOptions.cs
+++ Code/Light.DependencyInjection/DataStructures/RegistrationDictionaryOptions.cs
@@ -2,11 +2,21 @@
 
 namespace Light.DependencyInjection.DataStructures
 {
+    /// <summary>
+    ///     Represents the options that configure the <see cref="IGrowBucketsStrategy" /> for the <see cref="RegistrationDictionary" />.
+    /// </summary>
     public struct RegistrationDictionaryOptions
     {
         private IGrowBucketsStrategy _growContainerStrategy;
-        public static readonly IGrowBucketsStrategy DefaultGrowContainerStrategy = new PrimeNumberLinearStrategy();
 
+        /// <summary>
+        ///     Gets the default grow buckets strategy which is an instance of <see cref="PrimeNumberLinearStrategy" />.
+        /// </summary>
+        public static readonly IGrowBucketsStrategy DefaultGrowBucketsStrategy = new PrimeNumberLinearStrategy();
+
+        /// <summary>
+        ///     Gets or sets the <see cref="IGrowBucketsStrategy" /> that is used internally to decide when to grow the number of buckets of the dictionary.
+        /// </summary>
         public IGrowBucketsStrategy GrowContainerStrategy
         {
             get { return _growContainerStrategy; }
@@ -17,11 +27,14 @@ namespace Light.DependencyInjection.DataStructures
             }
         }
 
+        /// <summary>
+        ///     Creates a new instance of <see cref="RegistrationDictionaryOptions" /> with the default values.
+        /// </summary>
         public static RegistrationDictionaryOptions Create()
         {
             return new RegistrationDictionaryOptions
                    {
-                       _growContainerStrategy = DefaultGrowContainerStrategy
+                       _growContainerStrategy = DefaultGrowBucketsStrategy
                    };
         }
     }
