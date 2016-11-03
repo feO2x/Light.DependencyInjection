1d9f7f3cb39210ab73642b7f44674c220571609e
Kenny Pflug <kenny.pflug@live.de>
2016-11-03 17:00:16 +0100

    fixed bug where external instance registrations did not overwrite existing type mappings

---
+++ Code/Light.DependencyInjection.Tests/ReplaceMappingTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Light.DependencyInjection.Tests
+{
+    public sealed class ReplaceMappingsTests : DefaultDependencyInjectionContainerTest
+    {
+        [Fact(DisplayName = "Clients must be able to replace existing registrations with one that registers an external instance.")]
+        public void ReplaceRegistrationWithInstance()
+        {
+            Container.RegisterSingleton<ImplementationA>(options => options.MapToAllImplementedInterfaces());
+
+            var implementationB = new ImplementationB();
+            var resolvedInstance = Container.RegisterInstance(implementationB, options => options.MapToAllImplementedInterfaces())
+                                            .Resolve<IAbstraction>();
+
+            resolvedInstance.Should().BeSameAs(implementationB);
+        }
+
+        public interface IAbstraction { }
+
+        public sealed class ImplementationA : IAbstraction { }
+
+        public sealed class ImplementationB : IAbstraction { }
+    }
+}
